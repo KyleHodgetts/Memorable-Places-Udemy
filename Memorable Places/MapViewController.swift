2c5eabc984d914e0079c74af448742bbf8589f95
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-09-16 18:22:47 +0200

    Show map onViewDidLoad

--- Memorable Places/MapViewController.swift
+++ Memorable Places/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Memorable Places
 //
 //  Created by Kyle Hodgetts on 16/09/16.
@@ -10,11 +10,31 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    // Zoom level: Number of degrees difference along latitude and longitude of map
+    let latDelta: CLLocationDegrees = 0.01
+    let lonDelta: CLLocationDegrees = 0.01
+    
+    var activePlace: Place? // Will be current location if nil
 
     @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        if activePlace == nil {
+            // Set to current users location
+        }
+        
+        let latitude: CLLocationDegrees = 52.231876
+        let longitude: CLLocationDegrees = 21.005798
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
         
         map.delegate = self
     }
