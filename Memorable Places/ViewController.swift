582b021fa42ff3afd84f1c3ae8803864e10c0784
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-09-16 16:37:51 +0200

    Initial Commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by Kyle Hodgetts on 16/09/16.
+//  Copyright © 2016 com.github.kylehodgetts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
